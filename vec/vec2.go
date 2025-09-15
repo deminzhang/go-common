76378656be3b25f7a076df4473c4ceec2ef70b22
zhangdemin <zhangdemin@ruyi.cn>
2025-09-15 09:55:28 +0800

    vec

--- vec/vec2.go
+++ vec/vec2.go
@@ -20,6 +20,10 @@ func (this *Vec2[T]) Set(x, y T) {
 	this.Y = y
 }
 
+func (this *Vec2[T]) Length() float64 {
+	return math.Sqrt(float64((this.X * this.X) + (this.Y * this.Y)))
+}
+
 func (this *Vec2[T]) Clone() Vec2[T] {
 	return Vec2[T]{X: this.X, Y: this.Y}
 }
@@ -34,9 +38,9 @@ func (this *Vec2[T]) Sub(v Vec2[T]) {
 	this.Y -= v.Y
 }
 
-func (this *Vec2[T]) Multiply(scalar T) {
-	this.X *= scalar
-	this.Y *= scalar
+func (this *Vec2[T]) Multiply(scalar float64) {
+	this.X = T(float64(this.X) * scalar)
+	this.Y = T(float64(this.Y) * scalar)
 }
 
 func (this *Vec2[T]) Divide(scalar T) {
@@ -44,89 +48,71 @@ func (this *Vec2[T]) Divide(scalar T) {
 		//panic("v/0！")
 		this.X = T(math.Inf(1))
 		this.Y = T(math.Inf(1))
+		return
 	}
 	this.X /= scalar
 	this.Y /= scalar
 }
 
+// 向量：分向量乘
 func (this *Vec2[T]) Scale(v Vec2[T]) {
 	this.X *= v.X
 	this.Y *= v.Y
 }
 
+// 向量：点积
 func (this *Vec2[T]) Dot(v Vec2[T]) T {
 	return this.X*v.X + this.Y*v.Y
 }
 
+// 向量：长度
 func (this *Vec2[T]) Magnitude() T {
-	return T(math.Sqrt(float64((this.X * this.X) + (this.Y * this.Y))))
+	return T(math.Sqrt(float64(this.X*this.X + this.Y*this.Y)))
 }
 
-func (this *Vec2[T]) MagnitudeSqr() T {
+// 向量：长度平方
+func (this *Vec2[T]) SqrMagnitude() T {
 	return this.X*this.X + this.Y*this.Y
 }
 
+// 向量：单位化 (0向量禁用)
 func (this *Vec2[T]) Normalize() {
-	mag := this.Magnitude()
-	if mag == 0 {
-		this.X = 0
-		this.Y = 0
+	l := this.Magnitude()
+	if l == 0 {
+		this.Set(1, 0)
 		return
 	}
-	this.X /= mag
-	this.Y /= mag
+	this.Divide(l)
 }
 
+// 向量：单位化值 (0向量禁用)
 func (this *Vec2[T]) Normalized() Vec2[T] {
-	mag := this.Magnitude()
-	if mag == 0 {
-		return Vec2[T]{X: 0, Y: 0}
+	v := this.Clone()
+	v.Normalize()
+	return v
+}
+
+// 朝向不变拉长度
+func (this *Vec2[T]) ScaleToLength(newLength float64) {
+	this.Normalize()
+	this.Multiply(newLength)
+}
+
+// 复制朝向定长
+func (this *Vec2[T]) ScaledToLength(newLength float64) Vec2[T] {
+	v := this.Clone()
+	v.ScaleToLength(newLength)
+	return v
+}
+
+func (this *Vec2[T]) MoveTowards(targetX T, targetY T, speed float64) Vec2[T] {
+	dir := Vec2[T]{X: targetX - this.X, Y: targetY - this.Y}
+	dis := dir.Length()
+	if dis <= speed {
+		return Vec2[T]{X: targetX, Y: targetY}
 	}
-	return Vec2[T]{X: this.X / mag, Y: this.Y / mag}
+	dir.Normalize()
+	dir.Multiply(speed)
+	newVec := Vec2[T]{X: this.X + dir.X, Y: this.Y + dir.Y}
+	return newVec
 }
-
-func (this *Vec2[T]) Distance(v Vec2[T]) T {
-	return T(math.Sqrt(float64((this.X-v.X)*(this.X-v.X) + (this.Y-v.Y)*(this.Y-v.Y))))
-}
-
-func (this *Vec2[T]) DistanceSqr(v Vec2[T]) T {
-	return (this.X-v.X)*(this.X-v.X) + (this.Y-v.Y)*(this.Y-v.Y)
-}
-
-func (this *Vec2[T]) Angle(v Vec2[T]) T {
-	return T(math.Atan2(float64(this.Y-v.Y), float64(this.X-v.X)))
-}
-
-func (this *Vec2[T]) AngleDeg(v Vec2[T]) T {
-	return T(math.Atan2(float64(this.Y-v.Y), float64(this.X-v.X)) * 180 / math.Pi)
-}
-
-func (this *Vec2[T]) Rotate(angle T) {
-	cos := T(math.Cos(float64(angle)))
-	sin := T(math.Sin(float64(angle)))
-	x := this.X*cos - this.Y*sin
-	y := this.X*sin + this.Y*cos
-	this.X = x
-	this.Y = y
-}
-
-func (this *Vec2[T]) RotateDeg(angle T) {
-	cos := T(math.Cos(float64(angle) * math.Pi / 180))
-	sin := T(math.Sin(float64(angle) * math.Pi / 180))
-	x := this.X*cos - this.Y*sin
-	y := this.X*sin + this.Y*cos
-	this.X = x
-	this.Y = y
-}
-
-func (this *Vec2[T]) ScaleToLength(newLength T) {
-	mag := this.Magnitude()
-	if mag == 0 {
-		this.X = 0
-		this.Y = 0
-		return
-	}
-	this.X *= newLength / mag
-	this.Y *= newLength / mag
-}
-
