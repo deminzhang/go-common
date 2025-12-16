480ae2e315b983ecbdaf9ca54f09a71cea75d5c3
zhangdemin <zhangdemin@ruyi.cn>
2025-12-16 10:47:10 +0800

    更简洁的事件

--- event/event.go
+++ event/event.go
@@ -1,210 +1,70 @@
 package event
 
 import (
-	"log"
+	"fmt"
 	"reflect"
-	"sync"
 )
 
-// Event 反射版静态事件
-// 因用反射,限init时Def,Reg,不作Unregister
-// 以常量为事件名
-// 线程安全暂无
-var lockReg bool //禁止后续注册,静态事件应在init期间注册好
+type ErrorMode int
 
-type Event struct {
-	id        int
-	tp        reflect.Type
-	caller    reflect.Value //总回调检查用
-	listeners []listener
-	argNum    int //参数数
+const (
+	PanicOnError ErrorMode = iota
+	LogErrors
+)
+
+var (
+	errorMode ErrorMode = PanicOnError
+	lockReg   bool      //禁止后续注册,静态事件应在init期间注册好
+)
+
+type Event[T any] struct {
+	//mu    sync.RWMutex 如果Reg有多线程,则需要加锁,如果Reg在init,则不需要加锁
+	cbs  []T
+	Call T
 }
 
-type Context interface {
-	Set(key string, val any)
-	Get(key string) any
-}
-
-type EventContext struct {
-	val map[string]any
-}
-
-func NewEventContextWithVal(k string, v any) *EventContext {
-	return &EventContext{
-		val: map[string]any{k: v},
-	}
-}
-
-func (c EventContext) Set(k string, v any) {
-	c.val[k] = v
-}
-func (c EventContext) Get(k string) any {
-	v, b := c.val[k]
-	if b {
-		return v
-	}
-	return nil
-}
-
-type listener struct {
-	caller reflect.Value //回调
-}
-
-func (e *Event) Reg(cb interface{}) {
-	t := reflect.TypeOf(cb)
-	if t.Kind() != reflect.Func {
-		log.Fatalf("Event.Reg %d #2 must be a func*, got %v", e.id, cb)
-	}
-	if e.tp != t {
-		log.Fatalln("Event.Reg type not equal:", e.id)
-	}
-	e.listeners = append(e.listeners, listener{
-		caller: reflect.ValueOf(cb),
-		//filter: newWhen,
-	})
-}
-
-// 构建反射调用参数
-func makeArgv(a ...interface{}) ([]reflect.Value, int) {
-	argNum := len(a)
-	in := make([]reflect.Value, argNum)
-	for k, v := range a {
-		in[k] = reflect.ValueOf(v)
-	}
-	return in, argNum
-}
-
-func (e *Event) Call(a ...interface{}) {
-	list := e.listeners
-	caller := e.caller
-	in, argNum := makeArgv(a...)
-	if argNum != e.argNum {
-		log.Fatalln("Event.Call params number error:", e.id, argNum, caller.Type().NumIn())
-		return
-	}
-	for _, l := range list {
-		l.caller.Call(in)
-	}
-}
-
-func (e *Event) GoCall(a ...interface{}) {
-	list := e.listeners
-	caller := e.caller
-	in, argn := makeArgv(a...)
-	if argn != e.argNum {
-		log.Fatalln("Event.Call params number error:", e.id, argn, caller.Type().NumIn())
-		return
-	}
-	for _, l := range list {
-		go l.caller.Call(in)
-	}
-}
-
-func (e *Event) GoCallWaitAll(a ...interface{}) {
-	list := e.listeners
-	caller := e.caller
-	num := len(list)
-	if num == 0 {
-		return
-	}
-	in, argNum := makeArgv(a...)
-	if argNum != e.argNum {
-		log.Fatalln("Event.Call params number error:", e.id, argNum, caller.Type().NumIn())
-		return
-	}
-	for _, l := range list {
-		go func() {
-			defer func() {
-				num--
-			}()
-			l.caller.Call(in)
-		}()
-	}
-	for num > 0 {
-	}
-}
-
-func NewEvent(id int, foo interface{}) *Event {
-	t := reflect.TypeOf(foo)
-	if t.Kind() != reflect.Func {
-		log.Fatalf("Event.Reg %d #2 must be a func", id)
-	}
-	call := reflect.ValueOf(foo)
-	return &Event{
-		id:     id,
-		tp:     t,
-		caller: call,
-		argNum: call.Type().NumIn(),
-	}
-}
-
-// 全局事件
-type eventList struct {
-	sync.Mutex
-	events map[int]*Event
-}
-
-var globalEvents eventList
-
-func init() {
-	globalEvents.events = make(map[int]*Event)
-}
-
-func (l *eventList) get(id int) *Event {
-	l.Lock()
-	defer l.Unlock()
-	return l.events[id]
-}
-
-func (l *eventList) add(id int, e *Event) {
-	l.Lock()
-	defer l.Unlock()
-	l.events[id] = e
-}
-
-// Reg Listener
-// 注册响应(事件名,回调) 以首次注册时函数类型为准 尽量init时全注册好 动态注册可能造成call时的不确定性
-func Reg(id int, cb interface{}) {
+func (e *Event[T]) Reg(cb T) {
 	if lockReg {
-		panic("static event had lock reg")
+		panic("static event had lock reg. must reg in init")
 	}
-	e := globalEvents.get(id)
-	if e == nil {
-		e = NewEvent(id, cb)
-		globalEvents.add(id, e)
-	}
-	e.Reg(cb)
+	e.cbs = append(e.cbs, cb)
 }
 
-// Call Dispatcher
-// 触发事件_同步串行(事件名,参数集)
-func Call(id int, a ...interface{}) {
-	e := globalEvents.get(id)
-	if e == nil {
-		return
-	}
-	e.Call(a...)
-}
-
-// GoCall 触发事件_异步并行(事件名,参数集)确定互不冲突可用
-func GoCall(id int, a ...interface{}) {
-	e := globalEvents.get(id)
-	if e == nil {
-		return
-	}
-	e.GoCall(a...)
-}
-
-// GoCallWaitAll 触发事件_同步并行(事件名,参数集)确定互不冲突可用
-func GoCallWaitAll(id int, a ...interface{}) {
-	e := globalEvents.get(id)
-	if e == nil {
-		return
-	}
-	e.GoCallWaitAll(a...)
-}
-
-// LockReg 锁定静态事件注册
 func LockReg() {
 	lockReg = true
+}
+func SetErrorMode(mode ErrorMode) {
+	errorMode = mode
+}
+func Def[T any]() *Event[T] {
+	cbType := reflect.TypeFor[T]()
+	if cbType.Kind() != reflect.Func {
+		panic("Event type parameter must be a function")
+	}
+	e := &Event[T]{
+		cbs: make([]T, 0, 1),
+	}
+	fn := reflect.MakeFunc(cbType, func(args []reflect.Value) []reflect.Value {
+		var ret []reflect.Value // 返回最后一个回调的返回值
+		for _, cb := range e.cbs {
+			func() {
+				defer func() {
+					switch errorMode {
+					case PanicOnError:
+						return
+					}
+					if r := recover(); r != nil {
+						switch errorMode {
+						case LogErrors:
+							fmt.Printf("Event callback panic: %v\n", r)
+						}
+					}
+				}()
+				ret = reflect.ValueOf(cb).Call(args)
+			}()
+		}
+		return ret
+	})
+	e.Call = fn.Interface().(T)
+	return e
 }
--- event/event_test.go
+++ event/event_test.go
@@ -7,70 +7,6 @@ import (
 	"github.com/deminzhang/go-common/event"
 )
 
-func TestEventExample(t *testing.T) {
-	//Test测试示例
-	fmt.Println(">>event0.sample")
-	//定义
-	const (
-		_EventTest  = -1
-		_EventTest2 = -2
-		_Game1      = -100
-		_Game2      = -101
-	)
-	//单响应
-	event.Reg(_EventTest, func() {
-		fmt.Println("_EventTest1")
-	})
-	event.Call(_EventTest)
-
-	//多响应
-	event.Reg(_EventTest2, func(s string, i int) {
-		fmt.Println("A", s, i)
-	})
-	//
-	//staticevent.When(nil, 100)
-	event.Reg(_EventTest2, func(s string, i int) {
-		fmt.Println("B", s, i+1)
-	})
-	event.Call(_EventTest2, "_EventTest2A", 100)
-	//staticevent.Call(_EventTest2, "_EventTest2B", 200)
-
-	//分组
-	event.RegGroup(_Game1, _EventTest, func(s string, i int) {
-		fmt.Println("C", s, i+1)
-	})
-	event.RegGroup(_Game1, _EventTest, func(s string, i int) {
-		fmt.Println("D", s, i+1)
-	})
-	event.CallGroup(_Game1, _EventTest, "_EventTest3A", 100)
-
-}
-
-func BenchmarkEvent(b *testing.B) {
-	if true { //0.0025~ ns/op
-		event.Reg(-1, func(s string, i int) {
-			fmt.Println(s, i+1)
-		})
-		event.Reg(-1, func(s string, i int) {
-			fmt.Println(s, i+2)
-		})
-		event.Call(-1, "_EventTest2A", 100)
-	} else { //0.0005~ ns/op
-		var list = []func(s string, i int){
-			func(s string, i int) {
-				fmt.Println(s, i+1)
-			},
-			func(s string, i int) {
-				fmt.Println(s, i+2)
-			},
-		}
-		for _, f := range list {
-			f("_EventTest2A", 100)
-		}
-
-	}
-}
-
 func TestEvent2(t *testing.T) {
 	// define.go
 	var (
@@ -102,7 +38,12 @@ func TestEvent2(t *testing.T) {
 	})
 	Event3.Reg(func() {
 		fmt.Println("Callback 2")
+		panic("testX")
 	})
+	event.LockReg() // 锁定注册，防止后续注册
+	// Event3.Reg(func() { // 此处会panic,因为已经锁定注册
+	// 	fmt.Println("Callback 3")
+	// })
 
 	// trigger.go
 	Event1.Call("_EventTest2A", 100)
